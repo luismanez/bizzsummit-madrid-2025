5b6c6b1b4b34fadd2bb53c360164de5da3358611
Luis Manez <luis.manez@outlook.com>
2025-09-26 12:25:38 +0200

    CopilotStudioAgent init

--- src/MagIA.Agent/Program.cs
+++ src/MagIA.Agent/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Agents.CopilotStudio.Client;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents.Copilot;
+
+string appClientId = "<your-app-client-id>";
+string tenantId = "<your-tenant-id>";
+string clientSecret = "<your-client-secret>";
+
+CopilotStudioConnectionSettings settings = new(tenantId, appClientId, clientSecret);
+settings.EnvironmentId = "agent-env-id";
+settings.SchemaName = "agent-id";
+
+CopilotClient copilotClient = CopilotStudioAgent.CreateClient(settings);
+
+CopilotStudioAgent agent = new(copilotClient);
+
+await foreach(ChatMessageContent message in agent.InvokeAsync("Hello, how are you?"))
+{
+    Console.Write(message.Content);
+}
